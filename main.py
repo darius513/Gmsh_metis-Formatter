51c9dee46413a022a1d95808554d18dc8bb61617
dariusYoung <darius513@163.com>
2022-07-05 20:24:10 +0800

    Add inp format file conversion

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import argparse
 import os
 
+files = {
+    "msh": {"start": '$Elements', "delete_count": 5, "Separator": " ", "begin_Separator": "\n"},
+    "inp": {"start": '*ELEMENT', "delete_count": 1, "Separator": ",", "begin_Separator": ", "}
+}
+
 
 def formatter(path):
+    ele_num = 0  # element number
+    file_inf = files.get(os.path.basename(path).split('.')[1], "Invalid file type")
+    file_name = os.path.basename(path).split('.')[0]
     with open(path, 'r', encoding='utf-8') as f:
         line = f.readline()
         skip = True
 
-        with open(os.path.basename(path).split('.')[0] + '_formatted' + '.txt', 'w+', encoding='utf-8') as new_f:
+        with open(file_name + '_formatted' + '.txt', 'w+', encoding='utf-8') as new_f:
             while line:
-                # filter
+                # Termination condition
+                if not skip and not line.split(file_inf.get("Separator"))[0].strip('\n ').isdigit():
+                    break
                 if skip:
-                    skip = False if line == '$Elements\n' else True
-                    if skip or not skip and line == '$Elements\n':
+                    skip = False if line.split(file_inf.get("begin_Separator"))[0] == file_inf.get("start") else True
+                    # Ignore the first line at the beginning of the element block
+                    if skip or not skip and line.split(file_inf.get("begin_Separator"))[0] == file_inf.get("start"):
                         line = f.readline()
                         continue
 
-                if line.rstrip('\n').isdigit():
-                    new_f.write(line)
-                element = line.split()
-                del element[0:5]
+                # Removing spaces from each node str of each element
+                element = list(map(lambda x: x.strip(), line.split(file_inf.get("Separator"))))
+                del element[0:file_inf.get("delete_count")]
                 if len(element):
+                    ele_num = ele_num + 1
                     new_f.write(" ".join(element) + '\n')
                 line = f.readline()
-        print('output file name: ', format(os.path.basename(path).split('.')[0] + '_formatted' + '.txt'))
+
+        # Number of elements written at the beginning of the file
+        with open(file_name + '_formatted' + '.txt', 'r+', encoding='utf-8') as new_f:
+            content = new_f.read()
+            new_f.seek(0, 0)
+            new_f.write(str(ele_num) + '\n' + content)
+
+        print('output file name: ', format(file_name + '_formatted' + '.txt'))
 
 
 def get_parameters():
